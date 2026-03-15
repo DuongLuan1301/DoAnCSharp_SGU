2743da635ea8d62c4b8318e9657a0bcb26671c50
Duong Luan <nduongluan2005@gmail.com>
2026-03-15 09:17:27 +0700

    debug

--- project_csharp_sgu/obj/Debug/net10.0-windows10.0.19041.0/win-x64/project_csharp_sgu.AssemblyInfo.cs
+++ project_csharp_sgu/obj/Debug/net10.0-windows10.0.19041.0/win-x64/project_csharp_sgu.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_csharp_sgu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+11e540f4e6e1e3b9425f1466fed6bdac3125d18a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e61e8d236011ce6f8f7ec6a3ba8b4841f61de4a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_csharp_sgu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_csharp_sgu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
