788af0c5b813d5b7937624ac0677deefee23d3a7
taiz-k5coder <huynhtuantaiz0311k5@gmail.com>
2026-04-15 23:32:34 +0700

    Merge

--- backend/DoAnCSharp_Api/Models/Poi.cs
+++ backend/DoAnCSharp_Api/Models/Poi.cs
@@ -1,7 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Text.Json.Serialization;
-using System.Collections.Generic;
 
 namespace DoAnCSharp_Api.Models
 {
@@ -9,54 +8,47 @@ namespace DoAnCSharp_Api.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        [JsonPropertyName("id")] 
-        public string Id { get; set; } = string.Empty;
+        public string Id { get; set; }
 
         [BsonElement("name")]
-        [JsonPropertyName("name")]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
 
         [BsonElement("address")]
-        [JsonPropertyName("address")]
-        public string Address { get; set; } = string.Empty;
+        public string Address { get; set; }
 
         [BsonElement("image")]
-        [JsonPropertyName("image")]
-        public string Image { get; set; } = string.Empty;
+        public string Image { get; set; }
 
         [BsonElement("lat")]
-        [JsonPropertyName("lat")]
         public double Lat { get; set; }
 
         [BsonElement("lng")]
-        [JsonPropertyName("lng")]
         public double Lng { get; set; }
-
+// 1. Lượt quét QR
         [BsonElement("qrScans")]
-        [JsonPropertyName("qrScans")]
+        [JsonPropertyName("qrScans")] // Thêm dòng này
         public int QrScans { get; set; } = 0;
 
+        // 2. Lượt nghe Audio
         [BsonElement("audioListens")]
-        [JsonPropertyName("audioListens")]
+        [JsonPropertyName("audioListens")] // Thêm dòng này
         public int AudioListens { get; set; } = 0;
 
+        // 3. Tổng lượt truy cập
         [BsonElement("views")]
-        [JsonPropertyName("views")]
+        [JsonPropertyName("views")] // Thêm dòng này
         public int Views { get; set; } = 0;
 
         [BsonElement("localizations")]
-        [JsonPropertyName("localizations")]
-        public List<Localization>? Localizations { get; set; }
+        public List<Localization>? Localizations { get; set; } // nullable
     }
 
     public class Localization
     {
         [BsonElement("lang")]
-        [JsonPropertyName("lang")]
-        public string Lang { get; set; } = string.Empty;
+        public string? Lang { get; set; }  // nullable
 
         [BsonElement("description")]
-        [JsonPropertyName("description")]
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } // nullable
     }
 }
